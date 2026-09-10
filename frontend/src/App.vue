c4ef63e78c402975bc5b93d8cc34540e82382f6f
Noah Scharrenberg <nscharrenberg+github@proton.me>
2026-09-10 19:53:31 +0200

    feat(frontend): add frontend scaffolding with pinia and vue-router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,9 @@
 <script lang="ts" setup>
 import HelloWorld from './components/HelloWorld.vue';
+import {useRoute} from "vue-router";
+import {onMounted} from "vue";
+
+const route = useRoute();
 </script>
 
 <template>
